3b33b2b2a201b9026830d1232020527f6426fe6a
AlexeyKrop <kropotin_aleksey@mail.ru>
2020-09-08 20:21:06 +0300

    fix

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    pauseOnHover: true,
+  });
+  $(".stories-slider").slick();
+});
